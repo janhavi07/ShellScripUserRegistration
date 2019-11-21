ec21d078efe3d8352e5b6c68a31a7df758ae16d5
janhavi07 <partejanhavi@gmail.com>
2019-11-21 10:07:52 +0530

    Added code to validate First Name of user

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 shopt --s extglob
 
+echo " Enter the First name: "
+read firstName
+firstNamePattern="^[A-Z]+[a-z]*$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+        echo "VALID USERNAME"
+else
+        echo "INVALID USERNAME"
+fi
+
